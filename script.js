6f3cbd7d39bb87216482b276ae33ee3d5b53d86a
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2024-12-12 19:01:06 -0800

    AI-Site Landing Page

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ var typed = new Typed("#text",{
     backSpeed: 50,
     loop:true,}
 );
+
+// BRANDS SWIPER
+
+var swiper=new Swiper('.brandswiper',{
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: true,
+});
